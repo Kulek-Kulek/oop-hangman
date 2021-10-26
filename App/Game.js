4f032bb6aa000a77b24f76aba059f765656f8854
unknown <lugowski.k@gmail.com>
2021-10-26 21:23:40 +0200

    add quote class and game logic

--- App/Game.js
+++ App/Game.js
@@ -1,4 +1,33 @@
+import { Quote } from './Quote.js';
+
 class Game {
+    currentStep = 0;
+    lastStep = [...document.querySelectorAll('.step')].length;
+
+
+    quotes = [
+        {
+            text: 'pan tadeusz',
+            category: 'utwór literacki'
+        },
+        {
+            text: 'ogniem i mieciem',
+            category: 'utwór literacki'
+        },
+        {
+            text: 'jak trwoga to do boga',
+            category: 'cytaty'
+        },
+        {
+            text: 'kto rano wstaje ten jest niewyspany',
+            category: 'cytaty'
+        },
+        {
+            text: 'czterej pancerni i pies',
+            category: 'tytuły filmów'
+        },
+    ];
+
     constructor({
         lettersWrapper,
         categoryWrapper,
@@ -9,20 +38,52 @@ class Game {
         this.categoryWrapper = categoryWrapper;
         this.wordWrapper = wordWrapper;
         this.outputWrapper = outputWrapper;
+
+        const quoteIndex = Math.floor(Math.random() * this.quotes.length);
+        const { text, category } = this.quotes[quoteIndex];
+        this.quote = new Quote(text);
+        this.categoryWrapper.textContent = category;
+
     }
 
-    letterClicked(letter) {
-        console.log(letter);
+    letterClicked(letter, e) {
+        if (this.quote.guess(letter)) {
+            this.drawQuote();
+        } else {
+            this.currentStep++;
+            [...document.querySelectorAll('.step')][this.currentStep].style.opacity = 1;
+        }
+
+        e.target.disabled = true;
     }
 
-    start() {
+    drawLetters() {
         for (let i = 10; i < 36; i++) {
             const btn = document.createElement('button');
             const letter = i.toString(36);
-            btn.innerHTML = letter;
-            btn.addEventListener('click', () => this.letterClicked(letter));
-            this.lettersWrapper.appendChild(btn)
+            btn.textContent = letter;
+            btn.addEventListener('click', (e) => this.letterClicked(letter, e));
+            this.lettersWrapper.appendChild(btn);
         }
+    }
+
+    drawQuote() {
+        const slogan = this.quote.getContent();
+        this.wordWrapper.textContent = slogan;
+    }
+
+    start() {
+        this.drawLetters();
+        this.drawQuote();
+        document.querySelector('.step').style.opacity = 1;
     }
 }
 
+const wrappers = {
+    lettersWrapper: document.querySelector('#letters'),
+    categoryWrapper: document.querySelector('#category'),
+    wordWrapper: document.querySelector('#word'),
+    outputWrapper: document.querySelector('#output'),
+}
+const game = new Game(wrappers);
+game.start();
